6c0b6358a4626a081bdfafcbee759f7ad7176171
Vlatko <vlatkooop@gmail.com>
2024-07-16 20:45:00 +0200

    modal and pressable exercise

--- App.tsx
+++ App.tsx
@@ -5,11 +5,27 @@
  * @format
  */
 
-import React from 'react';
-import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Button,
+  Image,
+  ImageBackground,
+  Modal,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const pizzaImg = require('./assets/pizza.png');
+
+const MyIcon = () => <Icon name="rocket" size={30} color="#900" />;
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [isVisible, setIsVisible] = useState(false);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? '#000' : '#fff',
@@ -21,7 +37,131 @@ function App(): React.JSX.Element {
         ...styles.container,
         backgroundColor: backgroundStyle.backgroundColor,
       }}>
-      <Text style={styles.text}>Hello World</Text>
+      <ScrollView
+        style={{
+          flex: 1,
+        }}
+        contentContainerStyle={{
+          padding: 40,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        showsVerticalScrollIndicator={true}>
+        <ImageBackground source={pizzaImg} style={styles.backgroundImage}>
+          <Text
+            style={{
+              ...styles.text,
+              color: 'white',
+              fontSize: 60,
+              textAlign: 'center',
+            }}>
+            Djesi lesi
+          </Text>
+        </ImageBackground>
+        <Button
+          title="Button"
+          onPress={() => console.log('Pressed')}
+          color={'#900'}
+        />
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <Pressable
+          onPress={() => setIsVisible(true)}
+          onLongPress={() => console.log('Long Pressed')}>
+          <Image source={pizzaImg} style={styles.image} />
+        </Pressable>
+
+        <Text style={styles.text}>
+          Cao <Text style={styles.bold}>Svete</Text>
+        </Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <MyIcon />
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          deleniti harum ut eum commodi voluptate, dignissimos dolor et adipisci
+          aspernatur neque expedita amet. Maiores voluptate, dolores doloremque
+          maxime totam voluptates.
+        </Text>
+        <Modal
+          visible={isVisible}
+          onRequestClose={() => setIsVisible(false)}
+          animationType="slide"
+          presentationStyle="pageSheet">
+          <View
+            style={{
+              margin: 20,
+              borderRadius: 40,
+              backgroundColor: 'slateblue',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              position: 'relative',
+            }}>
+            <Text style={{fontSize: 40, color: 'white'}}>Hello</Text>
+            <Pressable
+              onPress={() => setIsVisible(false)}
+              style={{
+                position: 'absolute',
+                top: 30,
+                right: 30,
+              }}>
+              <Icon name="close" size={30} color="white" />
+            </Pressable>
+          </View>
+        </Modal>
+      </ScrollView>
     </View>
   );
 }
@@ -31,11 +171,27 @@ const styles = StyleSheet.create({
     borderColor: 'plum',
     borderWidth: 5,
     flex: 1,
+    gap: 30,
     justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
     fontSize: 20,
+    padding: 10,
+    borderBottomColor: 'slateblue',
+    borderBottomWidth: 5,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  backgroundImage: {
+    width: '100%',
+    height: 200,
+    padding: 60,
   },
 });
 
