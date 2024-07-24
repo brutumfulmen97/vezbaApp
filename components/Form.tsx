e7458009cfe6a201a3faec6d44b3d53bc9f00bbf
Vlatko <vlatkooop@gmail.com>
2024-07-24 18:34:08 +0200

    multiline text input

--- components/Form.tsx
+++ components/Form.tsx
@@ -10,11 +10,36 @@ import {
 
 const Form = () => {
   const [name, setName] = useState('');
+  const [message, setMesasge] = useState('');
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Unesite ime"
+        secureTextEntry={false}
+        keyboardType="default"
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardAppearance="dark"
+      />
+      <TextInput
+        style={[styles.input, styles.multiLineText]}
+        value={message}
+        onChangeText={setMesasge}
+        placeholder="Unesite poruku"
+        secureTextEntry={false}
+        keyboardType="default"
+        autoCapitalize="sentences"
+        autoCorrect={false}
+        keyboardAppearance="dark"
+        multiline={true}
+        numberOfLines={4}
+      />
       <Text style={styles.text}>Moje ime je: {name}</Text>
+      <Text style={styles.text}>Moja poruka je: {message}</Text>
     </SafeAreaView>
   );
 };
@@ -24,6 +49,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f2f2f2',
     paddingTop: StatusBar.currentHeight || 0,
+  },
+  multiLineText: {
+    textAlignVertical: 'top',
+    minHeight: 100,
   },
   text: {
     fontSize: 24,
